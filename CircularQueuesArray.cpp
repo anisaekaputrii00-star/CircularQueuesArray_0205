062d3505017e757a10632637d6a5e59f165bb367
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-05-26 11:35:30 +0700

    tambahkan operasi display

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -138,4 +138,9 @@ int main()
                      q.remove();
                     break;
                 }
+                case '3':
+                {
+                    q.display();
+                    break;
+                }
                 
